a741b326aff124b180c78ae0d7715d5ae1f8aa35
Adam Klimko <exusar@gmail.com>
2017-11-16 17:48:07 +0100

    Added data models

---
+++ app/src/main/java/pl/adamklimko/kkpandroid/model/BoughtProducts.java
@@ -0,0 +1,49 @@
+package pl.adamklimko.kkpandroid.model;
+
+public class BoughtProducts {
+    private int toiletPaper;
+    private int dishSoap;
+    private int trashBag;
+    private int soap;
+    private int sugar;
+
+    public int getToiletPaper() {
+        return toiletPaper;
+    }
+
+    public void setToiletPaper(int toiletPaper) {
+        this.toiletPaper = toiletPaper;
+    }
+
+    public int getDishSoap() {
+        return dishSoap;
+    }
+
+    public void setDishSoap(int dishSoap) {
+        this.dishSoap = dishSoap;
+    }
+
+    public int getTrashBag() {
+        return trashBag;
+    }
+
+    public void setTrashBag(int trashBag) {
+        this.trashBag = trashBag;
+    }
+
+    public int getSoap() {
+        return soap;
+    }
+
+    public void setSoap(int soap) {
+        this.soap = soap;
+    }
+
+    public int getSugar() {
+        return sugar;
+    }
+
+    public void setSugar(int sugar) {
+        this.sugar = sugar;
+    }
+}
---
+++ app/src/main/java/pl/adamklimko/kkpandroid/model/CleanedRooms.java
@@ -0,0 +1,31 @@
+package pl.adamklimko.kkpandroid.model;
+
+public class CleanedRooms {
+    private int bathroom;
+    private int kitchen;
+    private int floor;
+
+    public int getBathroom() {
+        return bathroom;
+    }
+
+    public void setBathroom(int bathroom) {
+        this.bathroom = bathroom;
+    }
+
+    public int getKitchen() {
+        return kitchen;
+    }
+
+    public void setKitchen(int kitchen) {
+        this.kitchen = kitchen;
+    }
+
+    public int getFloor() {
+        return floor;
+    }
+
+    public void setFloor(int floor) {
+        this.floor = floor;
+    }
+}
